3448dabdf5d4b0bd12212202432de7400465d4cb
LTowner <LTowner622@gmail.com>
2025-05-24 13:39:23 -0400

    Initial setup: Vite + React + Tailwind + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-6 bg-blue-500 text-white text-xl rounded-xl shadow-lg">
+      IronStack is ready.
+    </div>
+  );
+}
+
+export default App
